dbb0a75568903530a41c4e940c02255222dc78f3
Dominic Nzube Simeon <simeondominic@gmail.com>
2023-07-03 13:09:31 +0100

    update app

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-var express = require("express"),
-    app     = express();
+var express      = require("express"),
+    nodemailer   = require("nodemailer"),
+    app          = express();
 
-
+//app config
 app.set("view engine", "ejs");
-
+app.use(express.static("public"));
+app.use(express.json());
+var PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res){
     res.render("home");
 });
+app.post("/", function(req, res){
+    console.log(req.body);
 
-app.get("/about", function(req, res){
-    res.render("about")
+    var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "simeondominic@gmail.com",
+            pass: "ksoowzlndvwbcosm"
+        }
+    })
+    var mailOptions = {
+        from: req.body.email,
+        to: "simeondominic@gmail.com",
+        subject: `Message From ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            res.send("error")
+            res.redirect("/");
+        } else{
+            console.log("Email Sent: " + info.response);
+            res.send("Email sent");
+            res.redirect("/");
+        }
+    })
 })
 
+// app.get("/about", function(req, res){
+//     res.render("about")
+// })
 
-app.listen(3000, function(){
+
+app.listen(PORT, function(){
     console.log("Server Has Started!");
 });
